3b8b1c52f709334f94178e5dd9ef468be3c9fd4d
Jake Gage <jake.gage@gmail.com>
2016-03-06 04:32:32 -0600

    Add import completion and quiet script import
    - script no longer echos list of imports after each import statement
        - requires specifying each package
    - `:import` command now shell-completes newly `:grab`bed classes

--- README.md
+++ README.md
@@ -16,19 +16,15 @@ I've found this little Command class useful to tide myself over until a proper `
 
 ```sh
 groovy:000> :load https://git.io/v2N1I
-===> java.util.Map
-===> java.util.Map, groovy.grape.Grape
-===> java.util.Map, groovy.grape.Grape, org.codehaus.groovy.tools.GrapeUtil
-===> java.util.Map, groovy.grape.Grape, org.codehaus.groovy.tools.GrapeUtil, org.codehaus.groovy.tools.shell.CommandSupport
 ===> true
 groovy:000> :register GrabCommand
 ===> true
-groovy:000> :grab commons-primitives:commons-primitives:1.0
-groovy:000> import org.apache.commons.collections.primitives.ArrayIntList
-===> java.util.Map, groovy.grape.Grape, org.codehaus.groovy.tools.GrapeUtil, org.codehaus.groovy.tools.shell.CommandSupport, org.apache.commons.collections.primitives.ArrayIntList
+groovy:000> :grab 'org.spockframework:spock-core:1.0-groovy-2.4'
+groovy:000> import spock.lang.Specification 
+===> spock.lang.Specification
+groovy:000> 
 ```
 
 ## TODO
 
-- remove import messages during load
 - enable CI build / test
--- src/main/groovy/GrabCommand.groovy
+++ src/main/groovy/GrabCommand.groovy
@@ -1,33 +1,54 @@
-import java.util.Map
-import groovy.grape.Grape
-import org.codehaus.groovy.tools.GrapeUtil
-import org.codehaus.groovy.tools.shell.CommandSupport
-
-class GrabCommand extends CommandSupport {
+class GrabCommand extends org.codehaus.groovy.tools.shell.CommandSupport {
 
     public static final String COMMAND_NAME = ':grab'
 
     final String description='Add a dependency to the execution environment'
-    final String usage='<dependency|(group name version>)'
+    final String usage='<dependency|(group name version>)>'
     final String help='Add a dependency to the Groovy Shell runtime environment.'
+
+    private static final java.util.prefs.Preferences PREFERENCES =
+        java.util.prefs.Preferences.userRoot().node("/org/codehaus/groovy/tools/shell");
 
     public GrabCommand(org.codehaus.groovy.tools.shell.Groovysh shell) {
         super(shell, COMMAND_NAME, ':g')
     }
 
     Object execute(List<String> args) {
+        validate(args)
+        grab(getDependency(args))
+        reloadImportCompletion()
+    }
+
+    private void validate(List<String> args) {
         if ( args?.size() != 1 && args?.size() != 3 ) {
             fail("usage: @|bold ${COMMAND_NAME}|@ ${usage}")
         }
-        def dep
+    }
+
+    private String getDependency(List<String> args) {
         if ( args.size() == 3 ) {
             def (group, name, version) = args
-            dep = "${group}:${name}:${version}"
+            "${group}:${name}:${version}"
         } else {
-            dep = args[0]
+            args[0]
         }
-        Map<String, Object> dependency = GrapeUtil.getIvyParts(dep)
-        Grape.grab(dependency)
+    }
+
+    private void grab(String dependency) {
+        Map<String, Object> dependencyMap = org.codehaus.groovy.tools.GrapeUtil.getIvyParts(dependency)
+        groovy.grape.GrapeIvy grapeIvy = groovy.grape.Grape.instance
+        grapeIvy.grab([classLoader: shell.interp.classLoader.parent,refObject: shell.interp], dependencyMap)
+    }
+
+    private void reloadImportCompletion() {
+        shell.packageHelper.preferenceChange(ignorePackageCompletion(true))
+        shell.packageHelper.preferenceChange(ignorePackageCompletion(false))
+    }
+
+    java.util.prefs.PreferenceChangeEvent ignorePackageCompletion(Boolean complete) {
+        new java.util.prefs.PreferenceChangeEvent(PREFERENCES,
+            org.codehaus.groovy.tools.shell.util.PackageHelper.IMPORT_COMPLETION_PREFERENCE_KEY,
+            complete as String)
     }
 
 }
